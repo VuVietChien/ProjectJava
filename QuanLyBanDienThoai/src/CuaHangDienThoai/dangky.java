9706d9e3e039fcd1e2f4238432d7702cb35ef0ad
Nguyen Thuan <thuanbin1108@gmail.com>
2021-09-20 23:00:21 +0700

    Sua file dang ky

--- QuanLyBanDienThoai/src/CuaHangDienThoai/dangky.java
+++ QuanLyBanDienThoai/src/CuaHangDienThoai/dangky.java
@@ -7,12 +7,12 @@ package CuaHangDienThoai;
 
 /**
  *
- * @author viết chiến
+ * @author thuan
  */
 public class dangky extends javax.swing.JFrame {
 
     /**
-     * Creates new form Register
+     * Creates new form dangky
      */
     public dangky() {
         initComponents();
@@ -27,87 +27,17 @@ public class dangky extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-<<<<<<< HEAD
-=======
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-<<<<<<< HEAD
-        jButton1 = new javax.swing.JButton();
-=======
->>>>>>> thuan-dev
->>>>>>> master
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("jLabel1");
-
-<<<<<<< HEAD
-=======
-        jLabel2.setText("Hello");
-
-        jLabel3.setText("UOYLO");
-
-<<<<<<< HEAD
-        jButton1.setText("jButton1");
-
-=======
->>>>>>> thuan-dev
->>>>>>> master
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jLabel3)
-                .addGap(69, 69, 69))
-            .addGroup(layout.createSequentialGroup()
-<<<<<<< HEAD
-                .addGap(167, 167, 167)
-                .addComponent(jLabel1)
-                .addContainerGap(199, Short.MAX_VALUE))
-=======
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(167, 167, 167)
-                        .addComponent(jLabel1))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(154, 154, 154)
-                        .addComponent(jLabel2))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(19, 19, 19)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(199, Short.MAX_VALUE))
-<<<<<<< HEAD
-=======
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jLabel3)
-                .addGap(69, 69, 69))
->>>>>>> thuan-dev
->>>>>>> master
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-<<<<<<< HEAD
-                .addContainerGap(275, Short.MAX_VALUE))
-=======
-                .addGap(76, 76, 76)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel3)
-<<<<<<< HEAD
-                .addGap(45, 45, 45)
-                .addComponent(jButton1)
-                .addContainerGap(97, Short.MAX_VALUE))
-=======
-                .addContainerGap(165, Short.MAX_VALUE))
->>>>>>> thuan-dev
->>>>>>> master
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
         pack();
@@ -139,7 +69,6 @@ public class dangky extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(dangky.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -150,12 +79,5 @@ public class dangky extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-<<<<<<< HEAD
-=======
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
->>>>>>> thuan-dev
     // End of variables declaration//GEN-END:variables
 }
